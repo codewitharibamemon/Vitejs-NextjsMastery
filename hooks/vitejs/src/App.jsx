d6f3b88e5d3b9fcc7414b39cd2192fa1a47bdc14
codewitharibamemon <codewitharibamemon@gmail.com>
2025-05-17 17:15:25 +0500

    usestate

---
+++ hooks/vitejs/src/App.jsx
@@ -0,0 +1,30 @@
+
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+ // 1,2,3,4 
+  return (
+    <>
+      <h2>Counter App : {count}</h2>
+      <button onClick={() => setCount(count + 1)}>Add</button>
+      <button onClick={() => setCount(count - 1)}>Sub</button>
+       <button onClick={() => setCount(0)}>Reset</button>
+    </>
+  )
+}
+
+export default App
+
+
+
+
+
+
+
+
+
+//REACT HOOK KIA HOTA H? 
+//USE STATE HOTA KIA HAI 
+// KISI VALUE KO YA DATA KO YAAD RKHNA (STORE KRNA) JO TIME KY SATH CHANGE HOSKTI HO
