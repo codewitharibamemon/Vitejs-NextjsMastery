644129cd57c3d478d918efc4607bb3f7f89bbc83
codewitharibamemon <codewitharibamemon@gmail.com>
2025-05-22 21:57:34 +0500

    nextjs

--- hooks/vitejs/src/App.jsx
+++ hooks/vitejs/src/App.jsx
@@ -1,23 +1,21 @@
 
-import { useState } from 'react'
-import './App.css'
+// import { useState } from 'react'
+// import './App.css'
 
-function App() {
-  const [count, setCount] = useState(0)
- // 1,2,3,4 
-  return (
-    <>
-      <h2>Counter App : {count}</h2>
-      <button onClick={() => setCount(count + 1)}>Add</button>
-      <button onClick={() => setCount(count - 1)}>Sub</button>
-       <button onClick={() => setCount(0)}>Reset</button>
-    </>
-  )
-}
+// function App() {
+//   const [count, setCount] = useState(0)
+//  // 1,2,3,4 
+//   return (
+//     <>
+//       <h2>Counter App : {count}</h2>
+//       <button onClick={() => setCount(count + 1)}>Add</button>
+//       <button onClick={() => setCount(count - 1)}>Sub</button>
+//        <button onClick={() => setCount(0)}>Reset</button>
+//     </>
+//   )
+// }
 
-export default App
-
-
+// export default App
 
 
 
@@ -25,6 +23,18 @@ export default App
 
 
 
-//REACT HOOK KIA HOTA H? 
-//USE STATE HOTA KIA HAI 
-// KISI VALUE KO YA DATA KO YAAD RKHNA (STORE KRNA) JO TIME KY SATH CHANGE HOSKTI HO
+
+
+// //REACT HOOK KIA HOTA H? 
+// //USE STATE HOTA KIA HAI 
+// // KISI VALUE KO YA DATA KO YAAD RKHNA (STORE KRNA) JO TIME KY SATH CHANGE HOSKTI HO
+
+
+
+//use effect ik function hai jo hum use krtay hain aisa kam perform krny ky liye 
+// jo normal render ky baad chahye hota hai
+
+//Data Lena h API SY 
+//Timer lagana 
+//event 
+//ya asi koi cheez jo page load hony ky baad chhaye ho
